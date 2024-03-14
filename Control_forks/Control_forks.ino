c021d0454f0fed910b12e6c4b7377296d52bf4ee
MOCmaniac <52349445+MOCmaniac@users.noreply.github.com>
2024-03-14 12:37:53 +0100

    Perimeter logic implemented and working

--- Control_forks/Control_forks.ino
+++ Control_forks/Control_forks.ino
@@ -16,29 +16,24 @@ const int dirPlantPin = 18;
 
 
 // defines servo pin number (from pin 3 to 8)
-unsigned long lastServoUpdate = 0;
 const int feedbackPin = 24;  // Tx J4, feedback from servo is on 3.3V
-const int SERVO_PIN[] = {3, 4, 5, 6}
-const int SERVO_ANGLE_MIN[] = {0, 0, 0, 0};
-const int SERVO_ANGLE_DEPLOYED[] = {0, 4, 8, 3};
-const int SERVO_ANGLE_MAX[] = {2, 4, 8, 3};
-const int SERVO_TARGET_ANGLE[] = {}
-const int angleRetracted = 0;
-const int angleDeployed = 155;  // if changes value need to change feedback too !
-int targetAngleTrans = angleDeployed;
-int targetAngleArm = 105;
-int angleArm = targetAngleArm;
-int currentAngleTrans = 0;
-int currentFeedbackTrans = 0;
-int angle = 0;
-int feedback = 0;
+const int SERVO_PIN[] = { 3, 4, 5, 6 };
+// Changing the rotation direction is done in servo.attach() by reversing the min pulse width and max pulse width
+const int SERVO_MIN[] = { 0, 0, 0, -50 };
+const int SERVO_DEPLOYED[] = { 155, 180, 105, 0 };  // if changes value fork SERVO_FORKS need to change feedback too !
+const int SERVO_MAX[] = { 170, 180, 115, 50 };      // SERVO_MAX[3] = -SERVO_MIN[3]
+int SERVO_TARGET[] = { 155, 0, 0, 90 };
+int SERVO_ALLOWED[] = { 0, 0, 0, 0 };
+
+unsigned long lastServoUpdate = 0;
 unsigned int timeout = 10000;
-int minFeedback = 600;   // Based on a 12 bits resolution
+int allowedFeedbackForks = 0;
+int angleMeasured = 0;
+int feedbackMeasured = 0;
+int minFeedback = 600;  // Based on a 12 bits resolution
+int deployedFeedback = 2225;
 int maxFeedback = 2485;  // 180° : 2485
-int deployedFeedback = 2170;
-int angleGripper = 180;
-int speedRange = 50;
-int speedWheel = 0;
+
 
 // µs1 = 32, µs2 = 31, µs3 = 30, µs4 = 27
 const int microswitchPotPin = 27;    // Green cable
@@ -47,17 +42,18 @@ const int microswitchPlantPin = 30;  // Blue cable
 
 // TMC2209 : interpolation to 256 microsteps
 const int homingSpeed = 5000;  // max found : 16000 steps/second
-const int forkSpeed = 10000;
+const int forkSpeed = 4000;
 const int forkAcceleration = 40e3;  // in steps/second²
 int homingCompleted = 0;
-int maxHeight = 155;
-int parkingHeight = 40;
+int maxHeight = 100;
+int parkingHeight = 50;
 
 const int stepsPerRev = 200;
 const int microstepping = 8;
 const float ratio = 7.5;
 const int steps = round(stepsPerRev * ratio * 1.55) * microstepping;  // Reduction 7.5:1
 
+int armHasPriority = 0;
 int plantHasPriority = 1;
 int targetPositionForkPlant = steps;
 int targetPositionForkPot = steps;
@@ -78,22 +74,6 @@ enum servo {
   SERVO_WHEEL
 };
 
-// States
-enum states {
-  FORKS,
-  ARM,
-  RETRACTED,
-  UNCALIBRATED
-};
-
-// Names should be the same as the states enum and in the same order !
-const char *stateNames[] = {
-  "Forks",
-  "Arm",
-  "Retracted",
-  "Uncalibrated"
-};
-
 
 // Serial input variables
 const byte numChars = 10;
@@ -110,12 +90,10 @@ PWMServo servoGripper;
 PWMServo servoArm;
 PWMServo servoWheel;
 
-states previousState = UNCALIBRATED;
-states state = UNCALIBRATED;
 
 void setup() {
   SERIAL_BEGIN(115200);
-  DBG_PRINTF("\nStarting\nState : %s\n", stateNames[state]);
+  //DBG_PRINTF("\nStarting\nState : %s\n", stateNames[state]);
 
   analogReadResolution(12);
   pinMode(LEDPin, OUTPUT);
@@ -152,44 +130,58 @@ void loop() {
     newData = false;
   }
 
+  handleSystem();
+
   readServo(0);
   writeServos();
-
-  handleState();
-
-
-  int posPlant = targetPositionForkPlant;
-  int posPot = targetPositionForkPot;
-  if (plantHasPriority) {
-    posPot = min(targetPositionForkPot, targetPositionForkPlant);
-  } else {
-    posPlant = max(targetPositionForkPot, targetPositionForkPlant);
-  }
-
-  stepperPlant.moveTo(posPlant);
-  stepperPot.moveTo(posPot);
 
   stepperPlant.run();
   stepperPot.run();
 }
 
-void handleState() {
-  switch (state) {
-    case UNCALIBRATED:
-      DBG_PRINTLN("State : uncalibrated");
-      break;
-    case ARM:
-      DBG_PRINTLN("State : arm");
-      break;
-    case FORKS:
-      DBG_PRINTLN("State : forks");
-      break;
-    case RETRACTED:
-      DBG_PRINTLN("State : retracted");
-      break;
-    default:
-      DBG_PRINTLN("State unknown");
-      break;
+
+void handleSystem() {
+  static int lastPriority = 0;
+  if (lastPriority != armHasPriority) {
+    lastPriority = armHasPriority;
+    DBG_PRINTF("Arm has priority : %s\n", armHasPriority ? "YES" : "NO");
+  }
+
+  if (armHasPriority) {
+    setTargetForkPlant(parkingHeight);
+    setTargetForkPot(parkingHeight);
+    stepperPlant.moveTo(targetPositionForkPlant);
+    stepperPot.moveTo(targetPositionForkPot);
+
+    if (!stepperPlant.isRunning() && !stepperPot.isRunning()) {
+      SERVO_TARGET[SERVO_FORKS] = SERVO_MIN[SERVO_FORKS];
+    }
+
+    if (SERVO_ALLOWED[SERVO_FORKS] > SERVO_MIN[SERVO_FORKS] + 5) {
+      // bad to directly write the allowed values
+      SERVO_ALLOWED[SERVO_ARM] = SERVO_MIN[SERVO_ARM];
+      SERVO_ALLOWED[SERVO_WHEEL] = SERVO_DEPLOYED[SERVO_WHEEL];
+    }
+  } else {  // fork has priority
+    SERVO_TARGET[SERVO_ARM] = SERVO_MIN[SERVO_ARM];
+    SERVO_TARGET[SERVO_WHEEL] = SERVO_DEPLOYED[SERVO_WHEEL];
+
+    if (SERVO_ALLOWED[SERVO_ARM] > SERVO_MIN[SERVO_ARM] + 3) {
+      // bad to directly write the allowed value
+      SERVO_ALLOWED[SERVO_FORKS] = SERVO_MIN[SERVO_FORKS];
+    }
+
+    if (SERVO_ALLOWED[SERVO_ARM] <= SERVO_MIN[SERVO_ARM]) {
+      int posPlant = targetPositionForkPlant;
+      int posPot = targetPositionForkPot;
+      if (plantHasPriority) {
+        posPot = min(targetPositionForkPot, targetPositionForkPlant);
+      } else {
+        posPlant = max(targetPositionForkPot, targetPositionForkPlant);
+      }
+      stepperPlant.moveTo(posPlant);
+      stepperPot.moveTo(posPot);
+    }
   }
 }
 
@@ -221,9 +213,11 @@ void setTargetForkPot(int height) {
  */
 void setTranslationForks(int translation) {
   if (translation == 0 || translation == 1) {
-    targetAngleTrans = (translation) ? angleDeployed : angleRetracted;
-  } else if (translation >= 2 && translation <= 180) {
-    targetAngleTrans = translation;
+    SERVO_TARGET[SERVO_FORKS] = (translation) ? SERVO_DEPLOYED[SERVO_FORKS] : SERVO_MIN[SERVO_FORKS];
+    armHasPriority = translation ? 0 : 1;
+  } else if (translation >= 2 && translation <= SERVO_MAX[SERVO_FORKS]) {
+    SERVO_TARGET[SERVO_FORKS] = translation;
+    armHasPriority = (translation > 5) ? 0 : 1;
   }
 }
 
@@ -233,9 +227,9 @@ void setTranslationForks(int translation) {
  */
 void setPositionGripper(int angle) {
   if (angle == 1 || angle == 0) {
-    angleGripper = angle ? 0 : 180;
+    SERVO_TARGET[SERVO_GRIPPER] = angle ? SERVO_DEPLOYED[SERVO_GRIPPER] : SERVO_MIN[SERVO_GRIPPER];
   } else if (angle >= 2 && angle <= 180) {
-    angleGripper = 180 - angle;
+    SERVO_TARGET[SERVO_GRIPPER] = angle;
   }
 }
 
@@ -245,9 +239,11 @@ void setPositionGripper(int angle) {
  */
 void setRotationArm(int angle) {
   if (angle == 0 || angle == 1) {
-    targetAngleArm = (angle) ? 0 : 105;
-  } else if (angle >= 2 && angle <= 105) {
-    targetAngleArm = 105 - angle;
+    SERVO_TARGET[SERVO_ARM] = (angle) ? SERVO_DEPLOYED[SERVO_ARM] : SERVO_MIN[SERVO_ARM];
+    armHasPriority = angle ? 1 : 0;
+  } else if (angle >= 2 && angle <= SERVO_DEPLOYED[SERVO_ARM]) {
+    SERVO_TARGET[SERVO_ARM] = angle;
+    armHasPriority = (angle > 5) ? 1 : 0;
   }
 }
 
@@ -258,14 +254,15 @@ void setRotationArm(int angle) {
  */
 void setSpeedWheel(int speed) {
   DBG_PRINTF("Speed : %d\n", speed);
-  if (speed >= -speedRange && speed <= speedRange) {
-    speedWheel = speed;
+  if (speed >= SERVO_MIN[SERVO_WHEEL] && speed <= SERVO_MAX[SERVO_WHEEL]) {
+    SERVO_TARGET[SERVO_WHEEL] = speed;
   }
 }
 
 /*
  * Read value from servo feedback at set frequency and update the angle and feedback value variables
  * forceUpdate = TRUE : forces the update
+ * TODO check if EMA could be a better solution to read the values
  */
 void readServo(int forceUpdate) {
   static unsigned long lastServoRead = 0;
@@ -282,8 +279,8 @@ void readServo(int forceUpdate) {
     FB = round(fb / 5);
   }
 
-  feedback = FB;
-  angle = map(FB, minFeedback, maxFeedback, 0, 180);  // map function does not constraint
+  feedbackMeasured = FB;
+  angleMeasured = map(FB, minFeedback, maxFeedback, 0, 180);  // map function does not constraint
 }
 
 void writeServos() {
@@ -292,48 +289,47 @@ void writeServos() {
   static int pwm = 0;
 
   unsigned long time = millis();
-  if (time - lastServoWrite > 3) {
+  if (time - lastServoWrite > 20) {  // 3
     lastServoWrite = time;
 
     // Servo translation forks
-    if (currentFeedbackTrans + 70 < feedback) {  // Pulls forks inward
-      currentAngleTrans++;
-    } else if (currentFeedbackTrans - 16 > feedback) {  // Push forks outward
-      currentAngleTrans--;
-    } else if (targetAngleTrans > currentAngleTrans) {
-      currentAngleTrans++;
-    } else if (targetAngleTrans < currentAngleTrans) {
-      currentAngleTrans--;
+    if (allowedFeedbackForks + 70 < feedbackMeasured) {  // Pulls forks inward
+      SERVO_ALLOWED[SERVO_FORKS]++;
+    } else if (allowedFeedbackForks - 16 > feedbackMeasured) {  // Push forks outward
+      SERVO_ALLOWED[SERVO_FORKS]--;
+    } else if (SERVO_TARGET[SERVO_FORKS] > SERVO_ALLOWED[SERVO_FORKS]) {
+      SERVO_ALLOWED[SERVO_FORKS]++;
+    } else if (SERVO_TARGET[SERVO_FORKS] < SERVO_ALLOWED[SERVO_FORKS]) {
+      SERVO_ALLOWED[SERVO_FORKS]--;
     }
-    currentAngleTrans = constrain(currentAngleTrans, 0, 180);
-    currentFeedbackTrans = angleToFb(currentAngleTrans);
-    servoForks.write(currentAngleTrans);
+    SERVO_ALLOWED[SERVO_FORKS] = constrain(SERVO_ALLOWED[SERVO_FORKS], SERVO_MIN[SERVO_FORKS], SERVO_MAX[SERVO_FORKS]);
+    allowedFeedbackForks = angleToFb(SERVO_ALLOWED[SERVO_FORKS]);
+    servoForks.write(SERVO_ALLOWED[SERVO_FORKS]);
 
     // Servo gripper
-    servoGripper.write(angleGripper);
+    servoGripper.write(SERVO_TARGET[SERVO_GRIPPER]);
 
     // Servo rotation arm
-    if (targetAngleArm > angleArm) {
-      angleArm++;
-    } else if (targetAngleArm < angleArm) {
-      angleArm--;
+    if (SERVO_TARGET[SERVO_ARM] > SERVO_ALLOWED[SERVO_ARM]) {
+      SERVO_ALLOWED[SERVO_ARM]++;
+    } else if (SERVO_TARGET[SERVO_ARM] < SERVO_ALLOWED[SERVO_ARM]) {
+      SERVO_ALLOWED[SERVO_ARM]--;
     }
-    servoArm.write(angleArm);
+    servoArm.write(SERVO_ALLOWED[SERVO_ARM]);
   }
 
+  time = millis();
   if (time - lastServoWheelWrite > 1) {
     lastServoWheelWrite = time;
 
     //DBG_PRINTF("pwm : %d ", pwm);
-    if (abs(speedWheel) > pwm) {
-      int value = (speedWheel > 0) ? 160 : 20;
+    if (abs(SERVO_TARGET[SERVO_WHEEL]) > pwm) {
+      int value = (SERVO_TARGET[SERVO_WHEEL] > 0) ? 160 : 20;
       servoWheel.write(value);
-      //DBG_PRINTLN("ON");
     } else {
       servoWheel.write(90);
-      //DBG_PRINTLN("OFF");
     }
-    pwm = (pwm + 1) % speedRange;
+    pwm = (pwm + 1) % SERVO_MAX[SERVO_WHEEL];
   }
 }
 
@@ -464,26 +460,28 @@ void setupSteppers() {
 }
 
 void setupServos() {
-  pinMode(servoForksPin, OUTPUT);
-  pinMode(servoGripperPin, OUTPUT);
-  pinMode(servoArmPin, OUTPUT);
-  pinMode(servoWheelPin, OUTPUT);
+  pinMode(SERVO_PIN[SERVO_FORKS], OUTPUT);
+  pinMode(SERVO_PIN[SERVO_GRIPPER], OUTPUT);
+  pinMode(SERVO_PIN[SERVO_ARM], OUTPUT);
+  pinMode(SERVO_PIN[SERVO_WHEEL], OUTPUT);
   pinMode(feedbackPin, INPUT);
 
-  servoForks.attach(servoForksPin, 500, 2500);
+  servoForks.attach(SERVO_PIN[SERVO_FORKS], 500, 2500);
   readServo(1);
-  currentAngleTrans = angle;
-  currentFeedbackTrans = feedback;
+  SERVO_ALLOWED[SERVO_FORKS] = angleMeasured;
+  allowedFeedbackForks = feedbackMeasured;
 
-  servoGripper.attach(servoGripperPin, 500, 2500);  // datasheet 500-2500
+  servoGripper.attach(SERVO_PIN[SERVO_GRIPPER], 2500, 500);  // datasheet 500-2500
 
-  servoArm.attach(servoArmPin, 800, 2320);  // datasheet 700-2300. Tested from 740 to 2380 without stalling.
+  //servoArm.attach(SERVO_PIN[SERVO_ARM], 2320, 800);  // datasheet 700-2300. Tested from 740 to 2380 without stalling.
+  servoArm.attach(SERVO_PIN[SERVO_ARM], 2500, 500);
 
-  servoWheel.attach(servoWheelPin, 1400, 1600);  // datasheet 500-2500
+
+  servoWheel.attach(SERVO_PIN[SERVO_WHEEL], 1400, 1600);  // datasheet 500-2500
 
   DBG_PRINTF("Min feedback : %d\t deployed feedback : %d\n", minFeedback, deployedFeedback);
-  calibration(&servoForks, angleRetracted, &minFeedback);
-  calibration(&servoForks, angleDeployed, &deployedFeedback);
+  calibration(&servoForks, SERVO_MIN[SERVO_FORKS], &minFeedback);
+  calibration(&servoForks, SERVO_DEPLOYED[SERVO_FORKS], &deployedFeedback);
   DBG_PRINTF("Min feedback : %d\t deployed feedback : %d\n", minFeedback, deployedFeedback);
 }
 
@@ -562,23 +560,23 @@ int homingMove(AccelStepper *stepper, int speed, int maxTravelledSteps, int pin,
 void calibration(PWMServo *servo, int a, int *oldFb) {
   DBG_PRINTF("\n\nStarting calibration for angle : %d, old feedback : %d\n", a, *oldFb);
   readServo(1);
-  DBG_PRINTF("Current angle : %d, current feedback : %d\n", angle, feedback);
+  DBG_PRINTF("Current angle : %d, current feedback : %d\n", angleMeasured, feedbackMeasured);
   unsigned long start = millis();
   unsigned long time = start;
-  float fb = feedback;
+  float fb = feedbackMeasured;
   float lastFb = *oldFb;
-  //servo.write(angle); // library already constrain written angle value
-  targetAngleTrans = a;
+  //servo.write(angleMeasured); // library already constrain written angle value
+  SERVO_TARGET[SERVO_FORKS] = a;
 
   float error = 20.0;
-  while ((abs(angle - a) > 1 || error > 0.01) && time < start + timeout) {
+  while ((abs(angleMeasured - a) > 1 || error > 0.01) && time < start + timeout) {
     writeServos();
     readServo(1);
     lastFb = fb;
-    fb = 0.98 * fb + 0.02 * feedback;
+    fb = 0.98 * fb + 0.02 * feedbackMeasured;
     error = abs(fb - lastFb);
     time = millis();
-    //DBG_PRINTF("angle : %d, error : %.3f, time : %d\n", angle, error, start+10000 - time);
+    //DBG_PRINTF("angleMeasured : %d, error : %.3f, time : %d\n", angleMeasured, error, start+10000 - time);
     delay(1);
   }
   time = millis() - start;
@@ -586,12 +584,12 @@ void calibration(PWMServo *servo, int a, int *oldFb) {
   delay(100);  // wait for the servo to stop moving
   readServo(1);
 
-  if (abs(*oldFb - feedback) < 25) {
-    *oldFb = feedback;
-    DBG_PRINTF("Calibration completed in %dms\n  New feedback value : %d\n", time, feedback);
+  if (abs(*oldFb - feedbackMeasured) < 25) {
+    *oldFb = feedbackMeasured;
+    DBG_PRINTF("Calibration completed in %dms\n  New feedback value : %d\n", time, feedbackMeasured);
   } else if (time >= timeout) {
     DBG_PRINTLN("Calibration failed : timeout");
   } else {
-    DBG_PRINTF("Calibration failed : value outside range : %d\n", feedback);
+    DBG_PRINTF("Calibration failed : value outside range : %d\n", feedbackMeasured);
   }
 }
